f40d106f9100c6c605110af09d10602bb9cba789
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-10-23 02:02:13 +0200

    doc

--- frontend/src/components/ExampleWebAppIcon.tsx
+++ frontend/src/components/ExampleWebAppIcon.tsx
@@ -1,3 +1,5 @@
 import logo from '../assets/example-icon.svg'
 
+// inkscape edit need to pass through svgo tool ( https://www.npmjs.com/package/svgo )
+
 export const ExampleWebAppIcon = () => <img alt='example-icon' src={logo} width='100%' />
--- frontend/src/components/ExampleWebAppLogo.tsx
+++ frontend/src/components/ExampleWebAppLogo.tsx
@@ -1,3 +1,5 @@
 import logo from '../assets/example-logo.svg'
 
+// inkscape edit need to pass through svgo tool ( https://www.npmjs.com/package/svgo )
+
 export const ExampleWebAppLogo = () => <img alt='example-logo' src={logo} width='100%' />
