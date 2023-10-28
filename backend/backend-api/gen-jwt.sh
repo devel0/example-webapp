2aa6c7ac9dc984129686ecf89a456d7fb62bc06e
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-10-28 11:42:11 +0200

    fix for local tool manifest

--- backend/backend-api/gen-jwt.sh
+++ backend/backend-api/gen-jwt.sh
@@ -3,7 +3,7 @@
 exdir=$(dirname $(readlink -f "$0"))
 
 function genJwt() {
-    dotnet-script eval 'using System.Security.Cryptography; using System.Text; var rsaKey = RSA.Create(); var privateKey = rsaKey.ExportRSAPrivateKey(); System.Console.WriteLine(Convert.ToBase64String(privateKey));'
+    dotnet script eval 'using System.Security.Cryptography; using System.Text; var rsaKey = RSA.Create(); var privateKey = rsaKey.ExportRSAPrivateKey(); System.Console.WriteLine(Convert.ToBase64String(privateKey));'
 }
 
 genJwt
